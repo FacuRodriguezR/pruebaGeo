d705939870be0bd39b4c4bfa8670fee16fa80759
Facundo Rodriguez <facurtprn@gmail.com>
2023-05-22 12:38:36 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	// Slider
+	
+	  
+	  $('.galeria').bxSlider({
+	    mode: 'fade',
+	    captions: false,
+	    slideWidth: 1200,
+	    responsive: true,
+	    pager: true
+	  });
+
+
+
+	});
